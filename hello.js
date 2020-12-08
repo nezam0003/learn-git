49d51de39b755a90ff5bb604370ca84627d701aa
nezam0003 <fhameedk.nezam@gmail.com>
2020-12-08 15:09:52 +0600

    added a new function called addTwoNumber

--- hello.js
+++ hello.js
@@ -4,6 +4,11 @@ const sayName = (name) => {
 const sayHi = () => {
     console.log(`hi!!! ${sayName('Nezam')}`);
 }
+const addTwoNumber = (number1, number2) => {
+    const result = number1 + number2;
+    console.log(result);
+}
 sayHi();
+addTwoNumber(5, 10)
 const myFavColor = ['red', 'green', 'blue'];
 console.log(myFavColor);
