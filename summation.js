d171545ca2e5517a6abd4d5a09e3e29a619d7022
nezam0003 <fhameedk.nezam@gmail.com>
2020-12-09 14:42:21 +0600

    summation done by using reduce method.

--- summation.js
+++ summation.js
@@ -1,7 +1,9 @@
-const numbers = [1, 2, 3, 4, 5];
+const numbers = [1, 2, 3, 4, 5, 6];
 
 function sum(n) {
-    let summation = 0;
+    const summation = n.reduce((prevValue, currentValue) => {
+        return prevValue + currentValue;
+    }, 0);
     return summation;
 }
 console.log(sum(numbers));
